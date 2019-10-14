691e2d375844a31893e51502a7c7657aeb2423f3
miraclezys <907896638@qq.com>
2019-10-14 14:44:38 +0800

    workbox-start-demo update

--- workbox-start-demo/sw.js
+++ workbox-start-demo/sw.js
@@ -1,13 +1,31 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
 if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
+   console.log(`Yay! Workbox is loaded 🎉`);
 } else {
-  console.log(`Boo! Workbox didn't load 😬`);
+   console.log(`Boo! Workbox didn't load 😬`);
 }
 
 // JS 请求: 网络优先
 workbox.routing.registerRoute(
     /\.js$/,
-    new workbox.strategies.NetworkFirst()
+    new workbox.strategies.NetworkFirst({
+      cacheName: 'workbox:js',
+    })
+);
+
+// CSS 请求：缓存优先
+workbox.routing.registerRoute(
+    /.*\.css/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'workbox:css',
+    })
+);
+
+// Image 请求：缓存优先
+workbox.routing.registerRoute(
+	/.*\.(?:png|jpg|jpeg|svg|gif)/,
+	new workbox.strategies.CacheFirst({
+	  cacheName: 'workbox:image',
+	})
 );
